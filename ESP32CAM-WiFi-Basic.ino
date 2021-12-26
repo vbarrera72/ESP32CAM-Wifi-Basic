c46f97aecb629cf78b702d7642a38d45602ee1d3
vbarrera72 <95736849+vbarrera72@users.noreply.github.com>
2021-12-26 12:30:30 -0600

    Update ESP32CAM-WiFi-Basic.ino

--- ESP32CAM-WiFi-Basic.ino
+++ ESP32CAM-WiFi-Basic.ino
@@ -21,8 +21,8 @@
 #include <WiFi.h>  // Biblioteca para manejar el WiFi del ESP32CAM
 
 // Datos de Red
-const char* ssid = "AXTEL XTREMO-18D6";  // Pon aquí el nombre de la red a la que deseas conectarte
-const char* password = "038C18D6";  // Escribe la contraseña de dicha red
+const char* ssid = "*******";  // Pon aquí el nombre de la red a la que deseas conectarte
+const char* password = "*******";  // Escribe la contraseña de dicha red
 
 // Objetos
 WiFiClient espClient; // Este objeto maneja las variables necesarias para una conexion WiFi
